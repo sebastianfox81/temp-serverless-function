4216513d9b43dec5cb0a1fb324692f033d33b409
Sebastian <sebastianfox81@gmail.com>
2022-05-21 21:39:31 -0400

    Added headers prop to basic-api serverless func

--- functions/2-basic-api.js
+++ functions/2-basic-api.js
@@ -3,6 +3,9 @@ const items = require('../assets/data')
 
 exports.handler = (event, context) => {
   return {
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    }
     statusCode: 200,
     body: JSON.stringify(items)
   }
